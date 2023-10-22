d79155c9e01d70c913616d8a09c7f075efed6108
Usuario <MquesaG>
2023-10-22 10:58:47 -0600

    Agregar archivos de proyecto.

---
+++ Examen/ClsEmpleado.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    internal class ClsEmpleado
+    {
+        int opcion = 0;
+        string[] nombre = new string[10];
+        string[] cedula = new string[10];
+        string[] direccion = new string[10];
+        string[] telefono = new string[10];
+        string[] salario = new string[10];
+
+
+        public void agregar()
+        {
+
+            int cant = 1;
+            for (int i = 0; i < cant; i++)
+            {
+                Console.Clear();
+                Console.WriteLine("Digite la cedula del empleado 1=", cant);
+                cedula[i] = Console.ReadLine();
+                Console.WriteLine("Digite el nombre del empleado 1=", cant);
+                nombre[i] = Console.ReadLine();
+                Console.WriteLine("Digite la direccion del empleado 1=", cant);
+                direccion[i] = Console.ReadLine();
+                Console.WriteLine("Digite el telefono del empleado 1=", cant);
+                telefono[i] = Console.ReadLine();
+                int.TryParse(Console.ReadLine(), out cant);
+                Console.WriteLine("Digite el salario del empleado 1=", cant);
+                salario[i] = Console.ReadLine();
+                int.TryParse(Console.ReadLine(), out cant);
+                cant++;
+            }
+
+
+        }
+        public void consultar()
+        {
+            int cant = 1;
+            string ced = "";
+            Console.Write("Digite la Cedula: ");
+            ced = Console.ReadLine();
+            for (int i = 0; i < cant; i++)
+            {
+                if (Equals(cedula[i]))
+                {
+                    Console.Write($"Nombre: {nombre[i]}");
+                    Console.Write($"cedula: {cedula[i]}");
+                    break;
+                }
+            }
+
+        }
+        public void modificar()
+        {
+            int cant = 1;
+
+            for (int i = 0; i < cant; i++)
+            {
+                if (Equals(cedula[i]))
+                {
+                    Console.Write("Digite un nombre: ");
+                    nombre[i] = Console.ReadLine();
+                    Console.Write("Digite la cedula: ");
+
+                    break;
+                }
+
+            }
+        }
+    }
+}
+     
+        
+
+        
+   
+
---
+++ Examen/ClsMenu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    internal class ClsMenu
+    {
+        static int opcion;
+
+       static public void menu(){
+            int opcion = 0;
+           
+            do
+            {
+                Console.WriteLine("1=Agregar empleados");
+                Console.WriteLine("2=Consultar empleados");
+                Console.WriteLine("3=Borrar empleados");
+                Console.WriteLine("4=Modificar empleados");
+                Console.WriteLine("5=Inicializar arreglos");
+                Console.WriteLine("6=Reportes");
+                Console.WriteLine("7=Salir");
+                Console.WriteLine("Ingrese un valor:");
+                int.TryParse(Console.ReadLine(), out opcion);
+                switch (opcion)
+                {
+                    case 1: break;
+                    case 2: break; 
+                    case 3: break; 
+                    case 4: break; 
+                    case 5: break; 
+                    case 6: break;
+                    default:
+                        break;
+                }
+
+
+
+            } while (opcion!=7);
+        
+        }
+
+
+    }
+}
